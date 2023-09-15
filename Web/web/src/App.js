cd7f8ad0b0450bf7c38f16aa3f8dbd2ab3c7fef6
Josue3108 <67967986+Josue3108@users.noreply.github.com>
2023-09-15 17:28:49 -0600

    Revert "Button"
    This reverts commit 5e777e9e26586e1dd1fefbe82f0e1409cd55037c.

--- Web/web/src/App.js
+++ Web/web/src/App.js
@@ -1,16 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
-import { Button } from 'react-bootstrap';
+import { Button } from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>
-          Maldito Diego!!!!
-        </h2>
-        <Button variant = "primary">Bootstrap btn</Button>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
