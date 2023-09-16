ec9f98e66814315c4b47617ad6303f4f32e546d9
Kearranza <cbkevin.ant@gmail.com>
2023-09-15 18:46:30 -0600

    Update App.js

--- Web/web/src/App.js
+++ Web/web/src/App.js
@@ -7,10 +7,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>
-          Maldito Diego!!!!
-        </h2>
-        <Button variant = "primary">Bootstrap btn</Button>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
