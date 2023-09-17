c9c15c0d85eef215e20b7f87bfa90902d76d0c8b
Kearranza <cbkevin.ant@gmail.com>
2023-09-17 02:26:37 -0600

    Codux no sirve:(

--- Web/src/_codux/boards/actions-admin/actions-admin.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { ActionsAdmin } from '../../../components/actions-admin/actions-admin';
-
-export default createBoard({
-    name: 'ActionsAdmin',
-    Board: () => <ActionsAdmin />,
-    isSnippet: true,
-});
--- Web/src/_codux/boards/incorporate-proyection/incorporate-proyection.board.tsx
+++
@@ -1,8 +0,0 @@
-import { createBoard } from '@wixc3/react-board';
-import { IncorporateProyection } from '../../../components/incorporate-proyection/incorporate-proyection';
-
-export default createBoard({
-    name: 'IncorporateProyection',
-    Board: () => <IncorporateProyection />,
-    isSnippet: true,
-});
