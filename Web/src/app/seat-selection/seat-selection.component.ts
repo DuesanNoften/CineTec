443fc01d14bf952781827a35ef107301a7e4b660
Kearranza <cbkevin.ant@gmail.com>
2023-09-18 13:09:52 -0600

    Cambio visual seat (proxima plantilla)

--- Web/src/app/seat-selection/seat-selection.component.ts
+++ Web/src/app/seat-selection/seat-selection.component.ts
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./seat-selection.component.css']
 })
 export class SeatSelectionComponent {
-  rows: number = 5;
-  columns: number = 5;
+  rows: number = 7;
+  columns: number = 7;
   rowsArray = new Array(this.rows).fill(null).map((_, i) => i + 1);
   columnsArray = new Array(this.columns).fill(null).map((_, i) => i + 1);
 }
