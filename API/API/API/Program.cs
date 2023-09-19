0317d9451bcfbd3abc2a2e075bbdfb764637cf12
Devem002 <vega.diego02@gmail.com>
2023-09-18 22:40:03 -0600

    APP

--- API/API/API/Program.cs
+++ API/API/API/Program.cs
@@ -1,3 +1,4 @@
+using API;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
---
+++ CineAPP/app/src/main/java/com/example/cinetec/peliculasScreen.java
@@ -0,0 +1,14 @@
+package com.example.cinetec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class peliculasScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_peliculas_screen);
+    }
+}
